de2d175f33c916b57ddbd5c0e88a84de156b2336
Harikeshav R <r.harikeshav@gmail.com>
2024-03-28 13:01:13 +0530

    Solve R-1.2

---
+++ Chapter 1 - Python Primer/Exercises/R-1.2.py
@@ -0,0 +1,17 @@
+"""
+Write a short Python function, is_even(k), that takes an integer value and
+returns True if k is even, and False otherwise. However, your function
+cannot use the multiplication, modulo, or division operators.
+"""
+
+
+def is_even(k):
+    while k > 0:
+        k -= 2
+
+    return bool(k + 1)
+
+
+if __name__ == '__main__':
+    print(is_even(90))
+    print(is_even(181))
