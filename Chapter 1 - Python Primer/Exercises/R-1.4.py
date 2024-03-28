51f99be20c6eaa1b8b5a9028611a825f216c96a2
Harikeshav R <r.harikeshav@gmail.com>
2024-03-28 13:03:56 +0530

    Solve R-1.4

---
+++ Chapter 1 - Python Primer/Exercises/R-1.4.py
@@ -0,0 +1,12 @@
+"""
+Write a short Python function that takes a positive integer n and returns
+the sum of the squares of all the positive integers smaller than n.
+"""
+
+
+def sum_of_squares(n):
+    return sum(i ** 2 for i in range(1, n))
+
+
+if __name__ == '__main__':
+    print(sum_of_squares(10))
