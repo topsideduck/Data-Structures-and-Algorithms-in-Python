a4eb629500d85c00a05a5b30aca0cfdc76e7ae75
Harikeshav R <r.harikeshav@gmail.com>
2024-03-28 14:54:12 +0530

    Solve 1.15

--- Chapter 1 - Python Primer/Exercises/1.15.py
+++ Chapter 1 - Python Primer/Exercises/1.15.py
@@ -1,1 +1,13 @@
+"""
+Write a Python function that takes a sequence of numbers and determines
+if all the numbers are different from each other (that is, they are distinct).
+"""
 
+
+def distinct_elements(lst):
+    return len(set(lst)) == len(lst)
+
+
+if __name__ == '__main__':
+    print(distinct_elements([1, 4, 554, 382]))
+    print(distinct_elements([12, 9872, 12, 76]))
