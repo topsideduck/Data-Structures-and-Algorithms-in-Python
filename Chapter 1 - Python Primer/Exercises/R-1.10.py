fe85bf9e23154c5cc385a6a8edacc6eb7a92c7f1
Harikeshav R <r.harikeshav@gmail.com>
2024-03-28 13:29:40 +0530

    Solve R-1.10

---
+++ Chapter 1 - Python Primer/Exercises/R-1.10.py
@@ -0,0 +1,7 @@
+"""
+What parameters should be sent to the range constructor, to produce a
+range with values 8, 6, 4, 2, 0, −2, −4, −6, −8?
+"""
+
+if __name__ == '__main__':
+    print(range(8, -9, -2))
