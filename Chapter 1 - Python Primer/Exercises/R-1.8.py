7ac3c0793a18c11efca13cda18ccc2a4f5aba46a
Harikeshav R <r.harikeshav@gmail.com>
2024-03-28 13:23:18 +0530

    Solve R-1.8

---
+++ Chapter 1 - Python Primer/Exercises/R-1.8.py
@@ -0,0 +1,9 @@
+"""
+Python allows negative integers to be used as indices into a sequence,
+such as a string. If string s has length n, and expression s[k] is used for
+index −n ≤ k < 0, what is the equivalent index j ≥ 0 such that s[j] references
+the same element?
+"""
+
+# s[-k] == s[len(s) - k]
+# j = len(s) - k
