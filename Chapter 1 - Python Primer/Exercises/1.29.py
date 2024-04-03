3c89708d2b2298fe3ac88d7db72cbd94864553f4
Harikeshav R <r.harikeshav@gmail.com>
2024-04-03 16:04:39 +0530

    Solve 1.29 efficiently

--- Chapter 1 - Python Primer/Exercises/1.29.py
+++ Chapter 1 - Python Primer/Exercises/1.29.py
@@ -3,16 +3,47 @@ Write a Python program that outputs all possible strings formed by using the
 characters 'c' , 'a' , 't' , 'd' , 'o' ,and 'g' exactly once.
 """
 
-import random
+
+# Inefficient
+#
+# import random
+#
+#
+# def factorial(n):
+#     product = 1
+#
+#     for i in range(2, n + 1):
+#         product *= i
+#
+#     return product
+#
+#
+# if __name__ == '__main__':
+#     char_list = ['c', 'a', 't', 'd', 'o', 'g']
+#
+#     word_list = []
+#
+#     while len(word_list) < factorial(len(char_list)):
+#         random.shuffle(char_list)
+#
+#         word = "".join(char_list)
+#
+#         if word not in word_list:
+#             word_list.append(word)
+#
+#     print(word_list)
 
 
-def factorial(n):
-    product = 1
+# Correct way (using recursion) (saw online)
 
-    for i in range(2, n + 1):
-        product *= i
+def permutations(l):
+    if len(l) == 1:
+        yield l
 
-    return product
+    else:
+        for i in range(len(l)):
+            for p in permutations(l[:i] + l[i + 1:]):
+                yield l[i] + "".join(p)
 
 
 if __name__ == '__main__':
@@ -20,12 +51,7 @@ if __name__ == '__main__':
 
     word_list = []
 
-    while len(word_list) < factorial(len(char_list)):
-        random.shuffle(char_list)
-
-        word = "".join(char_list)
-
-        if word not in word_list:
-            word_list.append(word)
+    for i in permutations(char_list):
+        word_list.append(i)
 
     print(word_list)
