78f9e77d6d2391771737a8c7c878ef6acbf39e7d
Harikeshav R <r.harikeshav@gmail.com>
2024-04-03 15:13:52 +0530

    Solve 1.23

---
+++ Chapter 1 - Python Primer/Exercises/1.23.py
@@ -0,0 +1,15 @@
+"""
+Give an example of a Python code fragment that attempts to write an element
+to a list based on an index that may be out of bounds. If that index is out of
+bounds, the program should catch the exception that results, and print the
+following error message:
+“Don’t try buffer overflow attacks in Python!”
+"""
+
+if __name__ == '__main__':
+    try:
+        l = [1, 2, 3]
+        print(l[3])
+
+    except IndexError:
+        print("Don’t try buffer overflow attacks in Python!")
